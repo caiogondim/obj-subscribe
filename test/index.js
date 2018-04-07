29f5fce561db5c51904b9f94bdab85e69fe0edbc
Caio Gondim <me@caiogondim.com>
2018-04-07 12:46:11 -0400

    Working proto

---
+++ test/index.js
@@ -0,0 +1,22 @@
+const objSubscribe = require('../lib')
+
+it('executes callback whenever object is changed', () => {
+  let count = 0
+  function callback() {
+    count++
+  }
+
+  const obj = objSubscribe({
+    a: 1,
+    b: 2
+  }, callback)
+
+  expect(obj.a).toEqual(1)
+  expect(obj.b).toEqual(2)
+  obj.a = 3
+  obj.b = 4
+  expect(obj.a).toEqual(3)
+  expect(obj.b).toEqual(4)
+
+  expect(count).toEqual(2)
+})
